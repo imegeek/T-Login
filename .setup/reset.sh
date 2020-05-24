b0b0716e70f6c41aa2f62cbdf55b5045dd5e36a4
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-05-24 23:40:26 +0530

    Update reset.sh

--- .setup/reset.sh
+++ .setup/reset.sh
@@ -1,1 +1,51 @@
+cd $HOME && cd ../usr/etc && rm log.py > /dev/null 2>&1
+cd $HOME && cd ../usr/etc && rm bash.bashrc > /dev/null 2>&1
+cd $HOME && cd ../usr/etc && rm zshrc > /dev/null 2>&1
+sleep 3.0
 
+#setup motd
+echo > motd
+echo 'Welcome to Termux!' >> motd
+echo >> motd
+echo 'Wiki:            https://wiki.termux.com' >> motd
+echo 'Community forum: https://termux.com/community' >> motd
+echo 'Gitter chat:     https://gitter.im/termux/termux' >> motd
+echo "IRC channel:     #termux on freenode" >> motd
+echo >> motd
+echo 'Working with packages:' >> motd
+echo >> motd
+echo '* Search packages:   pkg search <query>' >> motd
+echo '* Install a package: pkg install <package>' >>  motd
+echo '* Upgrade packages:  pkg upgrade' >> motd
+echo >> motd
+echo 'Subscribing to additional repositories:' >> motd
+echo >> motd
+echo '* Root: pkg install root-repo' >> motd
+echo '* Unstable: pkg install unstable-repo' >> motd
+echo '* X11:      pkg install x11-repo' >> motd
+echo >> motd
+echo 'Report issues at https://termux.com/issues' >> motd
+echo >> motd
+
+#default bash.bashrc
+echo 'if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then' > bash.bashrc
+echo '        command_not_found_handle() {' >> bash.bashrc
+echo '                /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"' >> bash.bashrc
+echo '        }' >> bash.bashrc
+echo 'fi' >> bash.bashrc
+echo >> bash.bashrc
+echo "PS1='\$ '" >> bash.bashrc
+
+#default zshrc
+echo '. /data/data/com.termux/files/usr/etc/profile' > zshrc
+echo 'command_not_found_handler() {' >> zshrc
+echo '        /data/data/com.termux/files/usr/libexec/termux/command-not-found $1' >> zshrc
+echo '}' >> zshrc
+echo '#set nomatch so *.sh would not error if no file is available' >> zshrc
+echo 'setopt +o nomatch' >> zshrc
+echo '. /data/data/com.termux/files/usr/etc/profile' >> zshrc
+echo "PS1='%# '" >> zshrc
+echo
+echo -e '\033[1;93m                        [✓] Reset Configure Successfully [✓]'
+echo
+exec sleep 1
