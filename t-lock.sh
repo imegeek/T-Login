4a9e66e9122199ebeb86aff2af07c89e61a2ba90
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-05-25 10:39:20 +0530

    Update t-lock.sh

--- t-lock.sh
+++ t-lock.sh
@@ -12,8 +12,7 @@ command -v toilet > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require toilet 
 command -v ruby > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require ruby but it's not installed, Now Installing.\e[0m"; pkg install ruby -y; clear; }
 command -v lolcat > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require lolcat but it's not installed, Now Installing.\e[0m"; gem install lolcat; clear; }
 command -v wget > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require wget but it's not installed, Now Installing.\e[0m"; pkg install wget -y; clear; }
-command -v nano > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require nano but it's not installed, Now Installing.\e[0m"; pkg install nano -y; clear; }
-command -v python > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require python but it's not installed, Now Installing.\e[0m"; pkg install python -y; clear; bash t-lock.sh; }
+command -v nano > /dev/null 2>&1 || { echo -e >&2 "\033[1;91mI require nano but it's not installed, Now Installing.\e[0m"; pkg install nano -y; clear; bash t-lock.sh; }
 
 sleep 0.5
 echo -e '
