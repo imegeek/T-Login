9afe413eeb8c1a106b209a1aa5f9fafa946fb56c
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-05-25 10:42:46 +0530

    Update t-lock.sh

--- t-lock.sh
+++ t-lock.sh
@@ -27,3 +27,29 @@ echo -e '
                     ╚═╝      ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝   ┗━━┓▖┃
                                                                     ┗━┛
 ' | lolcat -a -t -d 1 -s 100
+
+echo
+sleep 0.8
+    printf "\e[1;96m                 [\e[0m\e[1;77m1\e[0m\e[1;96m]\e[0m\e[1;96m Start\e[1;94m  [\e[0m\e[1;77m2\e[0m\e[1;94m]\e[0m\e[1;94m Update\e[1;91m  [\e[0m\e[1;77m3\e[0m\e[1;91m]\e[0m\e[1;91m Reset to Default\e[1;97m [\e[0m\e[1;77m4\e[0m\e[1;97m]\e[0m\e[1;97m Exit\e[0m\n"
+ printf $'\n\e[1;92m                                [\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m'
+    read input
+    if [[ $input = "1" ]] || [[ $input = "01" ]]
+        then clear
+cd .setup && bash start.sh
+
+elif [[ $input = "2" ]] || [[ $input = "02" ]]
+        then clear
+cd .setup && bash update.sh
+
+elif [[ $input = "3" ]] || [[ $input = "03" ]]
+        then cd .setup && bash reset.sh
+
+elif [[ $input = "4" ]] || [[ $input = "04" ]]
+        then exit
+
+else
+echo
+printf "\e[1;95m                          [!] Invalid option! ~Restarting..\e[0m\n"
+sleep 0.8
+bash t-lock.sh
+fi
