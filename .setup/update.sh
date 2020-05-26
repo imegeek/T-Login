0a02bb138a8021b472b05d48e6a6938f5ff00550
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-05-26 11:21:27 +0530

    Update update.sh

--- .setup/update.sh
+++ .setup/update.sh
@@ -4,24 +4,9 @@ echo '
                                    ║ ║├─┘ ││├─┤ │ ├┤
                                    ╚═╝┴  ─┴┘┴ ┴ ┴ └─┘' | lolcat -t -a -d 1 -s 100
 echo
-echo -ne '        [⬇] T-Login Updating... |████|                            (10%)\r' | lolcat -t
+echo -ne '        [⬇] T-Lock Updating... |████|                            (10%)\r' | lolcat -t
 sleep 0.1
-echo -ne '        [⬇] T-Login Updating... |██████|                          (20%)\r' | lolcat -t
-sleep 0.1
-connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
-if [[ "$connection" != 0 ]]
-then clear
-echo '
-                                   ╦ ╦┌─┐┌┬┐┌─┐┌┬┐┌─┐
-                                   ║ ║├─┘ ││├─┤ │ ├┤
-                                   ╚═╝┴  ─┴┘┴ ┴ ┴ └─┘' | lolcat -t -a -d 1 -s 100
-
-   echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
-   exit
-fi
-echo -ne '        [⬇] T-Login Updating... |████████|                        (30%)\r' | lolcat -t
-sleep 0.1
-echo -ne '        [⬇] T-Login Updating... |███████████|                     (40%)\r' | lolcat -t
+echo -ne '        [⬇] T-Lock Updating... |██████|                          (20%)\r' | lolcat -t
 sleep 0.1
 connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
@@ -34,24 +19,9 @@ echo '
    echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
    exit
 fi
-echo -ne '        [⬇] T-Login Updating... |██████████████|                  (50%)\r' | lolcat -t
+echo -ne '        [⬇] T-Lock Updating... |████████|                        (30%)\r' | lolcat -t
 sleep 0.1
-echo -ne '        [⬇] T-Login Updating... |█████████████████|               (60%)\r' | lolcat -t
-sleep 0.1
-connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
-if [[ "$connection" != 0 ]]
-then clear
-echo '
-                                   ╦ ╦┌─┐┌┬┐┌─┐┌┬┐┌─┐
-                                   ║ ║├─┘ ││├─┤ │ ├┤
-                                   ╚═╝┴  ─┴┘┴ ┴ ┴ └─┘' | lolcat -t -a -d 1 -s 100
-
-   echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
-   exit
-fi
-echo -ne '        [⬇] T-Login Updating... |█████████████████████|           (70%)\r' | lolcat -t
-sleep 0.1
-echo -ne '        [⬇] T-Login Updating... |████████████████████████|        (80%)\r' | lolcat -t
+echo -ne '        [⬇] T-Lock Updating... |███████████|                     (40%)\r' | lolcat -t
 sleep 0.1
 connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
@@ -64,9 +34,39 @@ echo '
    echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
    exit
 fi
-echo -ne '        [⬇] T-Login Updating... |██████████████████████████|      (90%)\r' | lolcat -t
+echo -ne '        [⬇] T-Lock Updating... |██████████████|                  (50%)\r' | lolcat -t
 sleep 0.1
-echo -ne '        [⬇] T-Login Updating... |████████████████████████████|    (99%)\r' | lolcat -t
+echo -ne '        [⬇] T-Lock Updating... |█████████████████|               (60%)\r' | lolcat -t
+sleep 0.1
+connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
+if [[ "$connection" != 0 ]]
+then clear
+echo '
+                                   ╦ ╦┌─┐┌┬┐┌─┐┌┬┐┌─┐
+                                   ║ ║├─┘ ││├─┤ │ ├┤
+                                   ╚═╝┴  ─┴┘┴ ┴ ┴ └─┘' | lolcat -t -a -d 1 -s 100
+
+   echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
+   exit
+fi
+echo -ne '        [⬇] T-Lock Updating... |█████████████████████|           (70%)\r' | lolcat -t
+sleep 0.1
+echo -ne '        [⬇] T-Lock Updating... |████████████████████████|        (80%)\r' | lolcat -t
+sleep 0.1
+connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
+if [[ "$connection" != 0 ]]
+then clear
+echo '
+                                   ╦ ╦┌─┐┌┬┐┌─┐┌┬┐┌─┐
+                                   ║ ║├─┘ ││├─┤ │ ├┤
+                                   ╚═╝┴  ─┴┘┴ ┴ ┴ └─┘' | lolcat -t -a -d 1 -s 100
+
+   echo -e "\033[1;91m                             [\033[1;92m-\033[1;91m] No Internet\033[1;92m connection!"
+   exit
+fi
+echo -ne '        [⬇] T-Lock Updating... |██████████████████████████|      (90%)\r' | lolcat -t
+sleep 0.1
+echo -ne '        [⬇] T-Lock Updating... |████████████████████████████|    (99%)\r' | lolcat -t
 connection="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)"
 if [[ "$connection" != 0 ]]
 then clear
